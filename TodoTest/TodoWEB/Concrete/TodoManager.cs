c2a6bd412fbb805ff79e17e9878eaf1e0fed958f
Apostol <Apostol@MAIN>
2016-10-07 14:34:01 +0500

    Создал реализацию ITodoManager

---
+++ TodoTest/TodoWEB/Concrete/TodoManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoDAL.Abstract;
+using TodoDAL.Models;
+using TodoWEB.Abstract;
+
+namespace TodoWEB.Concrete
+{
+    public class TodoManager:ITodoManager
+    {
+        private readonly IRepository<Todo> _todoRepository;
+
+        public TodoManager(IRepository<Todo> todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        public async Task<IEnumerable<Todo>> GetListAsync(int userId)
+        {
+            return (await _todoRepository.GetListAsync()).Where(x => x.UserId == userId);
+        }
+
+        public async Task<Todo> GetItemAsync(int id)
+        {
+            return await _todoRepository.GetItemAsync(id);
+        }
+
+        public async Task UpdateAsync(Todo item)
+        {
+            await _todoRepository.UpdateAsync(item);
+            await _todoRepository.SaveAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _todoRepository.DeleteAsync(id);
+        }
+    }
+}
