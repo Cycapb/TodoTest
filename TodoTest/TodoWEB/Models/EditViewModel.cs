c9500dfb1985693fc3529e826c3912e800a6442b
Apostol <Apostol@MAIN>
2016-10-09 18:21:08 +0500

    Добавил атрибуты к свойствам модели EditViewModel. Добавил кнопку "На главную" во вьюшки Add и Edit. Отрисовку модели Todo вынес в PartialView _TodoItem.

--- TodoTest/TodoWEB/Models/EditViewModel.cs
+++ TodoTest/TodoWEB/Models/EditViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace TodoWEB.Models
 {
     public class EditViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+        [Display(Name = "Описание")]
+        [Required(ErrorMessage = "Не задано описание")]
         public string Description { get; set; }
+        [Display(Name = "Дата выполнения")]
         public DateTime DtEnd { get; set; }
     }
 }
