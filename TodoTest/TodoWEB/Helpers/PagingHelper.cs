71f252f0b41a23ac1975f7a505cbbab712a0c3e8
Apostol <Apostol@MAIN>
2016-10-10 19:04:36 +0500

    В PagingHelper добавил генерецию стрелок Вперед и Назад.

--- TodoTest/TodoWEB/Helpers/PagingHelper.cs
+++ TodoTest/TodoWEB/Helpers/PagingHelper.cs
@@ -10,6 +10,10 @@ namespace TodoWEB.Helpers
         public static MvcHtmlString CreatePages(this AjaxHelper htmlHelper, PagingInfo pagingInfo, Func<int, string> pageUrl)
         {
             StringBuilder result = new StringBuilder();
+            if (pagingInfo.CurrentPage > 1)
+            {
+                result.Append(CreateArrow(false, pagingInfo.CurrentPage, pageUrl));
+            }
             for (int i = 1; i <= pagingInfo.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
@@ -27,7 +31,25 @@ namespace TodoWEB.Helpers
                 tag.MergeAttribute("data-ajax-url",pageUrl(i));
                 result.Append(tag.ToString());
             }
+            if (pagingInfo.CurrentPage < pagingInfo.TotalPages)
+            {
+                result.Append(CreateArrow(true, pagingInfo.CurrentPage, pageUrl));
+            }
+
             return MvcHtmlString.Create(result.ToString());
+        }
+
+        private static string CreateArrow(bool forward, int currentPage,Func<int, string> pageUrl)
+        {
+            TagBuilder nextTag = new TagBuilder("a");
+            nextTag.MergeAttribute("href", pageUrl(forward ? currentPage++ : currentPage--));
+            nextTag.InnerHtml = forward? ">>" : "<<";
+            nextTag.AddCssClass("btn btn-default");
+            nextTag.MergeAttribute("data-ajax", "true");
+            nextTag.MergeAttribute("data-ajax-mode", "replace");
+            nextTag.MergeAttribute("data-ajax-update", "#todoPanel");
+            nextTag.MergeAttribute("data-ajax-url", pageUrl(forward ? currentPage++ : currentPage--));
+            return nextTag.ToString();
         }
     }
 }
