ee04eec6d72cba88850dc61e29de4deb87eccac9
Apostol <Apostol@MAIN>
2016-10-10 18:35:32 +0500

    Кнопка Добавить становится неактивной при редактировании задачи. При выходе из редактирования - активируется.

--- TodoTest/TodoWEB/Scripts/Scripts.js
+++ TodoTest/TodoWEB/Scripts/Scripts.js
@@ -1,3 +1,11 @@
 ﻿function clearTextbox() {
     $("#query").val("");
 }
+
+function disableAdd() {
+    $("#btnAdd").prop("disabled",true);
+}
+
+function enableAdd() {
+    $("#btnAdd").prop("disabled", false);
+}
